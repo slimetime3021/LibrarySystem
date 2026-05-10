926689134980550ae43899694a9657686bb8b640
derick <derick.burjack.cardoso@gmail.com>
2026-05-10 13:35:42 -0400

    Added fields, constructor, ToString, Equals and Hash code for Item class

--- src/main/java/org/derick/domain/Item.java
+++ src/main/java/org/derick/domain/Item.java
@@ -1,4 +1,25 @@
 package org.derick.domain;
 
+import lombok.*;
+
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Getter
 public abstract class Item {
+    protected String Id;
+    @Setter protected String title;
+    @Setter protected Status status;
+
+    private static int nextId = 1;
+
+    public Item(String title, Status status) {
+        this.Id = String.format("%04d", nextId++);
+        this.title = title;
+        this.status = status;
+    }
+
+    public enum Status {
+        BORROWED, IN_STORE, LOST
+    }
 }
