02ee558757ebf6ec24d736b7af9151f12047bad5
derick <derick.burjack.cardoso@gmail.com>
2026-05-10 16:53:29 -0400

    Added fields and constructors for DVD

---
+++ src/main/java/org/derick/domain/DVD.java
@@ -0,0 +1,23 @@
+package org.derick.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@EqualsAndHashCode(callSuper=true)
+@ToString(callSuper=true)
+public class DVD extends Item {
+    private String director;
+    private int minutes;
+
+    public DVD(String Id, String title, Status status, String director, int minutes) {
+        super(Id, title, status);
+        this.director = director;
+        this.minutes = minutes;
+    }
+
+    public DVD(String title, Status status, String director, int minutes) {
+        super(title, status);
+        this.director = director;
+        this.minutes = minutes;
+    }
+}
