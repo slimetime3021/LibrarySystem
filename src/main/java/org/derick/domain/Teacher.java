4e128316f8c99a47a09144ae473fd74feda38f10
derick <derick.burjack.cardoso@gmail.com>
2026-05-10 14:06:41 -0400

    Added ToString, EqualsAndHashCode, constructors and borrowing limit class constant and its getter for borrowing limit

---
+++ src/main/java/org/derick/domain/Teacher.java
@@ -0,0 +1,25 @@
+package org.derick.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.util.List;
+
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Teacher extends User {
+    private static final int borrowingLimit = 10;
+
+    public Teacher(String Id, String name, List<Item> borrowedItems) {
+        super(Id, name, borrowedItems);
+    }
+
+    public Teacher(String name, List<Item> borrowedItems) {
+        super(name, borrowedItems);
+    }
+
+    @Override
+    public int getBorrowingLimit() {
+        return borrowingLimit;
+    }
+}
